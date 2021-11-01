a1dc0871b4528de4471acd0a577b5cf4ea7ebd6b
Andrei <andreim.graur@gmail.com>
2021-11-01 13:50:50 +0200

    Checking log in info against the database

--- data_handler.py
+++ data_handler.py
@@ -16,3 +16,36 @@ def register_user(cursor: RealDictCursor, username: str, password: str) -> list:
         VALUES ('{username}', '{password}', '{CURRENT_TIME}')"""
     cursor.execute(query)
 
+@database_common.connection_handler
+def check_login(cursor: RealDictCursor, username: str, password: str) -> list:
+    wrong_username = f"User {username} not found!"
+    wrong_password = "Wrong password!"
+
+    query = f"""SELECT username 
+                FROM users
+                WHERE username = '{username}'"""
+
+    cursor.execute(query)
+
+    try:
+        fetch_username = dict(cursor.fetchone())
+    except TypeError:
+        return wrong_username
+        
+    if fetch_username is None:
+        return wrong_username
+    else:
+        query = f"""SELECT password
+                    FROM users
+                    WHERE username = '{username}'"""
+ 
+        cursor.execute(query)
+        fetch_password = dict(cursor.fetchone())
+
+        hashed_user_password = fetch_password["password"].encode('utf-8')
+        check_hash = bcrypt.checkpw(password.encode("utf-8"), hashed_user_password)
+
+        if check_hash is False:
+            return wrong_password
+        return check_hash
+
