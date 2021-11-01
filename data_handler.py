1b66a2e188042cdcab29d20e9ea7a32f414bc06f
Andrei <andreim.graur@gmail.com>
2021-11-01 13:23:37 +0200

    Hashing password for users

--- data_handler.py
+++ data_handler.py
@@ -1,6 +1,13 @@
 import database_common
+import bcrypt
 from psycopg2 import sql
 from psycopg2.extras import RealDictCursor
+
+
+def hash_password(password):
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_password.decode('utf-8')
+
 
 @database_common.connection_handler
 def register_user(cursor: RealDictCursor, username: str, password: str) -> list:
